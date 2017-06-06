1e7b044fb12678101b069a1d3b08c01b7c38b5e8
Yusen <tianyusen@outlook.com>
2017-06-05 22:19:46 -0700

    Init Selenium test file

---
+++ stest.js
@@ -0,0 +1,10 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+driver.get('http://localhost:3000');
+driver.quit();
